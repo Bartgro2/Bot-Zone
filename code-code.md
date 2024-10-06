c3e71d11e71363bc520a7b016cfa4798730ea603
Bart <akiraenishi@gmail.com>
2024-10-06 15:44:01 +0200

    test adjustment code (ban, warn, kick) with extra json

---
+++ code-code.md
@@ -0,0 +1,34 @@
+
+# Current commands
+
+moderation
+
+- (ban)
+- json(ban)
+- (kick)
+- json(kick)
+- (warn)
+- (clear)
+
+- moderation-actions.json (reasons, for kick, ban warnings, etc)
+
+misc
+- (help)
+- (info)
+
+Data(Json)
+embeds.json
+moderation-actions.json
+warnings.json
+
+## To-do
+
+### patchnodes
+
+2-10-2024:
+
+- adjusted the ban command to have better explantions for banning someone
+
+- made a kick command use to ban.js command as base and slighy adjusted it.
+  
+- made a simple warning command that counts warning (which can results in a ban/kick) 3 warnings is ban. (need to adjust this in the future )
--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,4 +1,5 @@
-const {Client, Interaction, ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js');
+const { Client, Interaction, ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js');
+const ActionData = require('../../jsons/moderation-actions.json'); // Load moderation actions from JSON
 
 module.exports = {
   /**
@@ -8,64 +9,76 @@ module.exports = {
    */
 
   callback: async (client, interaction) => {
-     const targetUserId = interaction.options.get('target-user').value;
-     const reason = interaction.options.get('reason')?.value || "No reason provided";
+    const targetUserId = interaction.options.get('target-user').value;
 
-     await interaction.deferReply();
+    // Attempt to get the custom reason; if none, default to "No reason provided"
+    const customReason = interaction.options.get('reason')?.value || "No reason provided";
 
-     const targetUser = await interaction.guild.members.fetch(targetUserId);
+    await interaction.deferReply();
 
-     if (!targetUser) {
-       await interaction.editReply("That user doesn't exist in this server.");
-       return;
-     }
+    const targetUser = await interaction.guild.members.fetch(targetUserId);
 
-     if (targetUser.id === interaction.guild.ownerId) {
-       await interaction.editReply("You can't ban this user cause they are the server owner.");
-       return;
-     }
+    if (!targetUser) {
+      await interaction.editReply("That user doesn't exist in this server.");
+      return;
+    }
 
-     const targetUserRolePosition = targetUser.roles.highest.position; //Highest role of the target user
-     const requestUserRolePosition = interaction.member.roles.highest.position //Highes role of the user running the command
-     const botRolePosition = interaction.guild.members.me.roles.highest.position; //Highest role of the bot
+    if (targetUser.id === interaction.guild.ownerId) {
+      await interaction.editReply("You can't ban this user because they are the server owner.");
+      return;
+    }
 
-     if (targetUserRolePosition >= requestUserRolePosition) {
-       await interaction.editReply("You can't ban this user cause they have the same/higher role then you.");
-       return;
-     }
+    const targetUserRolePosition = targetUser.roles.highest.position; // Highest role of the target user
+    const requestUserRolePosition = interaction.member.roles.highest.position; // Highest role of the user running the command
+    const botRolePosition = interaction.guild.members.me.roles.highest.position; // Highest role of the bot
 
-     if (targetUserRolePosition >= botRolePosition) {
-       await interaction.editReply("I can't ban this user cause they have the same/higher role then me.");
-       return;
-     }
-    
-     // Ban the targetUser
+    if (targetUserRolePosition >= requestUserRolePosition) {
+      await interaction.editReply("You can't ban this user because they have the same or a higher role than you.");
+      return;
+    }
 
-     try {
-       await targetUser.ban({ reason });
-       await interaction.editReply(`User ${targetUser} was banned\nReason: ${reason}`);
-     } catch (error) {
-       console.log(`There was an error when banning: ${error}`)
-     }
+    if (targetUserRolePosition >= botRolePosition) {
+      await interaction.editReply("I can't ban this user because they have the same or a higher role than me.");
+      return;
+    }
+
+    // Determine the reason to be used for banning
+    let reason = customReason;
+
+    if (ActionData.reasons[customReason] && ActionData.reasons[customReason].applicableActions.includes('ban')) {
+      reason = ActionData.reasons[customReason].description; // Use predefined reason if provided and applicable to ban
+    } else {
+      reason = "Custom Reason: " + customReason; // Label as custom if not in predefined reasons or not applicable to ban
+    }
+
+    // Ban the target user
+    try {
+      await targetUser.ban({ reason });
+      await interaction.editReply(`User ${targetUser} was banned.\nReason: ${reason}`);
+    } catch (error) {
+      console.log(`There was an error when banning: ${error}`);
+      await interaction.editReply("An error occurred while trying to ban the user.");
+    }
   },
-  
-    name: 'ban',
-    description: 'Bans a member from the server',
-    //devOnly: Boolean,
-    //testOnly Boolean,
-    options: [
-        {
-           name: 'target-user',
-           description: 'The user you want to ban',
-           required: true, 
-           type: ApplicationCommandOptionType.Mentionable
-        },
-        {
-            name: 'reason',
-            description: 'The reason for banning',
-            type: ApplicationCommandOptionType.String,
-         },
-    ],
-    permissionsRequired: [PermissionFlagsBits.BanMembers],
-    botPremissions: [PermissionFlagsBits.BanMembers], 
-}
+
+  name: 'ban',
+  description: 'Bans a member from the server',
+  options: [
+    {
+      name: 'target-user',
+      description: 'The user you want to ban',
+      required: true,
+      type: ApplicationCommandOptionType.Mentionable
+    },
+    {
+      name: 'reason',
+      description: 'The reason for banning',
+      type: ApplicationCommandOptionType.String,
+      choices: Object.keys(ActionData.reasons)
+        .filter(reason => ActionData.reasons[reason].applicableActions.includes('ban'))
+        .map(reason => ({ name: reason, value: reason })),
+    },
+  ],
+  permissionsRequired: [PermissionFlagsBits.BanMembers],
+  botPermissions: [PermissionFlagsBits.BanMembers],
+};
