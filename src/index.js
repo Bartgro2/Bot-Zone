ca1cda0eb75c8eccf1a80b8b048c83462013984b
Bart <akiraenishi@gmail.com>
2024-12-04 09:32:00 +0100

    adjustment, removed unneeded code

--- src/index.js
+++ src/index.js
@@ -14,34 +14,3 @@ const client = new Client({
 eventHandler(client);
 
 client.login(process.env.TOKEN);
-
-/*
-client.on('ready', (c) => {
-    console.log(`${c.user.tag} is online.`);
-});
-
-client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isButton()) return; // Handle button interactions specifically
-
-    try {
-        await interaction.deferReply({ ephemeral: true });
-      
-        const role = interaction.guild.roles.cache.get(interaction.customId);
-        if (!role) {
-            await interaction.editReply("I couldn't find that role");
-            return;
-        }
-
-        const hasRole = interaction.member.roles.cache.has(role.id);
-
-        if (hasRole) {
-            await interaction.member.roles.remove(role); // Remove the role if the user has it
-            await interaction.editReply(`The role ${role.name} has been removed.`);
-        } else {
-            await interaction.member.roles.add(role); // Add the role if the user doesn't have it
-            await interaction.editReply(`The role ${role.name} has been added.`);
-        }
-    } catch (error) {
-        console.error('Error handling interaction:', error);
-    }
-});*/
